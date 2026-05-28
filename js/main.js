97c4a62595569df709cc2b7df3dc3272d1048304
parksangseok <park0157@naver.com>
2026-05-28 16:13:46 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -9,4 +9,22 @@ $(function () {
     $('#itemSpecSelectWrap').removeClass('active');
   });
 
+  //스크롤 그라데이션 
+  $('.itemListTabContentList').each(function () {
+    const $w = $(this);
+
+    $w.scroll(function () {
+      const st = $w.scrollTop();
+      const h = $w.innerHeight();
+      const sh = this.scrollHeight;
+
+      if (sh > h + 1) {
+        $w.addClass('hasScroll')
+          .toggleClass('scrollEnd', st + h >= sh - 5);
+      } else {
+        $w.removeClass('hasScroll scrollEnd');
+      }
+    }).trigger('scroll');
+  });
+
 });
